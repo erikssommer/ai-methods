fb55ce7d860d409e1fbc41cad421867bbb39398c
Erik Storås Sommer <erikssommer@gmail.com>
2023-01-21 15:12:58 +0100

    Added comments to part 2

--- uncertainty/prob_same_birthday.py
+++ uncertainty/prob_same_birthday.py
@@ -2,33 +2,8 @@ import random
 import math
 
 
-def same_birthday(n):
-    same_birthday = 0
-    iterations = 10000
-
-    for _ in range(iterations):
-        birthdays = []
-        for _ in range(n):
-            birthdays.append(random.randint(1, 365))
-        if len(birthdays) != len(set(birthdays)):
-            same_birthday += 1
-    return same_birthday / iterations
-
-
-def make_group():
-    group = []
-    days = set(range(1, 365))
-    counter = 0
-    while len(days) > 0:
-        birthday = random.randint(1, 365)
-        group.append(birthday)
-        counter += 1
-        days.discard(birthday)
-    return counter
-
-
 def task1():
-    # the value of N when the probability will be greater than 50%
+    # The value of N when the probability will be greater than 50%
     n_values = range(10, 50)
     prob_min_50 = 0
     min_n = None
@@ -47,12 +22,27 @@ def task1():
     print("The smallest N where the probability of the event occurring is at least 50% is:", min_n)
 
 
+def same_birthday(n):
+    same_birthday = 0
+    iterations = 10000
+
+    for _ in range(iterations):
+        birthdays = []
+        for _ in range(n):
+            birthdays.append(random.randint(1, 365))
+        if len(birthdays) != len(set(birthdays)):
+            same_birthday += 1
+    return same_birthday / iterations
+
+
 def task2():
+    # The number of trials to run
     trials = 1000
     total_people = 0
     for _ in range(trials):
         total_people += make_group()
 
+    # The average number of people to add to the group
     average_people = total_people / trials
 
     # Rounding up to the nearest integer
@@ -60,6 +50,25 @@ def task2():
           math.ceil(average_people))
 
 
+def make_group():
+    group = []
+    days = set(range(1, 365))
+    counter = 0
+    # Iterate until all days are used
+    while len(days) > 0:
+        # Pick a random day as a birthday
+        birthday = random.randint(1, 365)
+        # Add the birthday to the group and increment the counter
+        group.append(birthday)
+        counter += 1
+        # Remove the birthday from the set of days
+        days.discard(birthday)
+
+    return counter
+
+
 if __name__ == "__main__":
+    print("------ Part 1 -------")
     task1()
+    print("------ Part 2 -------")
     task2()
