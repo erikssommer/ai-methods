4ee2d8b0ca4ffd87485cb834a809a55c8ac1414a
Erik Storås Sommer <erikssommer@gmail.com>
2023-02-03 23:19:29 +0100

    Converting to numpy array and printing it

--- prob_reasoning_over_time/prob_reasoning_over_time.py
+++ prob_reasoning_over_time/prob_reasoning_over_time.py
@@ -1,22 +1,21 @@
 import numpy as np
 
-
+# Normalize a matrix
 def normalize(matrix):
     return matrix / np.sum(matrix)
 
 
+# Forward algorithm
 def forward(T, O, prob):
     return normalize(np.dot(np.dot(O, T.T), prob))
 
 
 def main():
-
     # Transition matrix
     T = np.array([[0.7, 0.3], [0.3, 0.7]])
 
     # Observation matrices
     O_true = np.array([[0.9, 0.0], [0.0, 0.2]])
-
     O_false = np.array([[0.1, 0.0], [0.0, 0.8]])
 
     # Observations given
@@ -25,10 +24,21 @@ def main():
     # Initial probability
     prob = np.array([0.5, 0.5])
 
-    for i, e in enumerate(evidence):
-        prob = forward(T, O_true, prob) if e else forward(T, O_false, prob)
-        print(f"Probability after {i + 1} observation(s): {prob}")
-        
+    # List of probabilities
+    probs = []
+    probs.append(prob)
+
+    # Compute probabilities
+    for _, value in enumerate(evidence):
+        prob = forward(T, O_true, prob) if value else forward(T, O_false, prob)
+        probs.append(prob)
+
+    # Convert to numpy array
+    probs = np.array(probs)
+
+    # Print probabilities
+    print(probs)
+
 
 if __name__ == "__main__":
     main()
