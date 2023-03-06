6a44bbe4a5db9ca41fcf858fdae8a706ed33eb8a
Erik Storås Sommer <erikssommer@gmail.com>
2023-03-06 23:52:31 +0100

    Formatting

--- neural_networks_intro/feedforward_nn.py
+++ neural_networks_intro/feedforward_nn.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def func(X: np.ndarray) -> np.ndarray:
     """
     The data generating function.
@@ -31,8 +32,11 @@ def get_data(n_train: int, n_test: int) -> tuple[np.ndarray, np.ndarray, np.ndar
     return X_train, y_train, X_test, y_test
 
 # Activation function
+
+
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
+
 
 class NeuralNetwork:
     def __init__(self, input_size, hidden_size, output_size, learning_rate):
@@ -42,8 +46,10 @@ class NeuralNetwork:
         self.learning_rate = learning_rate
 
         # Initialize weights with random values from a normal distribution
-        self.weights_input_hidden = np.random.normal(0.0, pow(self.input_size, -0.5), (self.hidden_size, self.input_size))
-        self.weights_hidden_output = np.random.normal(0.0, pow(self.hidden_size, -0.5), (self.output_size, self.hidden_size))
+        self.weights_input_hidden = np.random.normal(
+            0.0, pow(self.input_size, -0.5), (self.hidden_size, self.input_size))
+        self.weights_hidden_output = np.random.normal(
+            0.0, pow(self.hidden_size, -0.5), (self.output_size, self.hidden_size))
 
     def forward(self, inputs):
         hidden_inputs = np.dot(self.weights_input_hidden, inputs)
@@ -65,11 +71,14 @@ class NeuralNetwork:
 
         # Backward pass
         output_errors = targets - final_outputs
-        hidden_errors = np.dot(self.weights_hidden_output.T, output_errors) * hidden_outputs * (1 - hidden_outputs)
+        hidden_errors = np.dot(self.weights_hidden_output.T,
+                               output_errors) * hidden_outputs * (1 - hidden_outputs)
 
         # Update weights
-        self.weights_hidden_output += self.learning_rate * np.dot(output_errors, hidden_outputs.T)
-        self.weights_input_hidden += self.learning_rate * np.dot(hidden_errors, inputs.T)
+        self.weights_hidden_output += self.learning_rate * \
+            np.dot(output_errors, hidden_outputs.T)
+        self.weights_input_hidden += self.learning_rate * \
+            np.dot(hidden_errors, inputs.T)
 
     def predict(self, inputs):
         inputs = inputs.reshape(((self.input_size, self.output_size)))
@@ -98,9 +107,9 @@ if __name__ == "__main__":
 
             nn.train(inputs, targets)
 
-
     # Evaluate on training set
-    y_train_pred = np.array([nn.predict(X_train[i]) for i in range(len(X_train))])
+    y_train_pred = np.array([nn.predict(X_train[i])
+                            for i in range(len(X_train))])
     mse_train = np.mean((y_train - y_train_pred) ** 2)
     print("MSE on training set:", mse_train)
 
@@ -108,20 +117,3 @@ if __name__ == "__main__":
     y_test_pred = np.array([nn.predict(X_test[i]) for i in range(len(X_test))])
     mse_test = np.mean((y_test - y_test_pred) ** 2)
     print("MSE on test set:", mse_test)
-
-    # Plot predictions
-    plt.scatter(y_train, y_train_pred)
-    plt.plot([0, 5], [0, 5], '--', color='gray')  # Add diagonal line for reference
-    plt.xlabel('True values')
-    plt.ylabel('Predicted values')
-    plt.title('Predicted vs True values (Training set)')
-    plt.show()
-
-    # Plot predicted vs true values for test set
-    plt.scatter(y_test, y_test_pred)
-    plt.plot([0, 5], [0, 5], '--', color='gray')  # Add diagonal line for reference
-    plt.xlabel('True values')
-    plt.ylabel('Predicted values')
-    plt.title('Predicted vs True values (Test set)')
-    plt.show()
-
