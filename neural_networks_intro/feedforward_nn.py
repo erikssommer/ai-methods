43f114ebb02ecf4a295705dce29a4aef29ac307e
Erik Storås Sommer <erikssommer@gmail.com>
2023-03-06 09:43:18 +0100

    Added starting code

---
+++ neural_networks_intro/feedforward_nn.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+
+def func(X: np.ndarray) -> np.ndarray:
+    """
+    The data generating function.
+    Do not modify this function.
+    """
+    return 0.3 * X[:, 0] + 0.6 * X[:, 1] ** 2
+
+
+def noisy_func(X: np.ndarray, epsilon: float = 0.075) -> np.ndarray:
+    """
+    Add Gaussian noise to the data generating function.
+    Do not modify this function.
+    """
+    return func(X) + np.random.randn(len(X)) * epsilon
+
+
+def get_data(n_train: int, n_test: int) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    """
+    Generating training and test data for
+    training and testing the neural network.
+    Do not modify this function.
+    """
+    X_train = np.random.rand(n_train, 2) * 2 - 1
+    y_train = noisy_func(X_train)
+    X_test = np.random.rand(n_test, 2) * 2 - 1
+    y_test = noisy_func(X_test)
+
+    return X_train, y_train, X_test, y_test
+
+
+if __name__ == "__main__":
+    np.random.seed(0)
+    X_train, y_train, X_test, y_test = get_data(n_train=280, n_test=120)
+
+    # TODO: Your code goes here.
