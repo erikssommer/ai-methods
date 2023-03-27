59bd0d0bff71f1d7d71d57febe62b24663d17482
Erik Storås Sommer <erikssommer@gmail.com>
2023-03-27 21:25:03 +0200

    Implemented value iteration fun and plot result

--- reinforcement_learning/value_iteration.py
+++ reinforcement_learning/value_iteration.py
@@ -1,4 +1,6 @@
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 rewards: tuple[float, ...] = (-0.1, -0.1, -0.1, -0.1,
@@ -123,7 +125,117 @@ def get_action_as_str(action: int) -> str:
     return ("left", "down", "right", "up")[action]
 
 
+def value_iteration() -> list[float]:
+    # Initialize the utilities of all states to 0
+    U = np.zeros(N_STATES)
+
+    # Run value iteration until convergence
+    while True:
+        # Initialize the change in utility for this iteration
+        delta = 0
+        # Update the utility of each state
+        for state in range(N_STATES):
+            # Calculate the new utility of the state
+            new_util = u_value(state, U)
+            # Update the change in utility if the new utility is better
+            delta = max(delta, abs(new_util - U[state]))
+            # Update the utility of this state
+            U[state] = new_util
+
+        # Check if the change in utility is small enough to stop iterating
+        if delta < (EPSILON * (1 - GAMMA)) / GAMMA:
+            break
+
+    # Scale and round the utilities to the desired format
+    U = np.round(U / 10, decimals=4)
+    # Convert the utilities to a list and return them
+    return U.tolist()
+
+
+def u_value(state: int, U: list[float]) -> float:
+    # Find the maximum q-value over all actions from this state
+    return max(q_value(state, action, U) for action in range(N_ACTIONS))
+
+
+def q_value(state: int, action: int, U: list[float]) -> float:
+    """ Calculate the maximum expected utility of a given state-action pair """
+    # Get the next possible states given the current state and action
+    next_states: list[int] = get_next_states(state, action)
+    # Initialize q-value to 0
+    q = 0
+    # Calculate the expected utility of each next state, taking into account the transition probabilities and rewards
+    for next_state in next_states:
+        trans_prob = get_trans_prob(state, action, next_state)
+        expected_utility = get_reward(next_state) + GAMMA * U[next_state]
+        q += trans_prob * expected_utility
+
+    # Return the weighted sum of the expected utilities, which is the q-value
+    return q
+
+
+def greedy_policy(state: int, U: list[float]) -> int:
+    # Create a list to store the Q-values for each action in the current state
+    q_values = []
+    # Loop through all possible actions in the current state and compute their Q-values
+    for action in range(N_ACTIONS):
+        q_values.append(q_value(state, action, U))
+
+    # Find the index of the maximum Q-value, which corresponds to the best action
+    best_action = np.argmax(q_values)
+    # Return the index of the best action
+    return best_action
+
+
+def display_utilities(utils: list[float]):
+    # Reshape the utilities into a 4x4 grid
+    grid = np.array(utils).reshape(4, 4)
+
+    # Create a heatmap of the utilities using Seaborn
+    sns.heatmap(grid, annot=True, fmt=".4f", cmap="YlGnBu")
+
+    # Add a title to the plot
+    plt.title("Utilities")
+
+    # Show the plot
+    plt.show()
+
+
+def display_greedy_policy(U: list[float]):
+    # Start at the initial state
+    curr_state = 0
+    # Create a 4x4 grid to hold the optimal actions
+    policy = np.zeros((4, 4))
+
+    # Continue until we reach the goal state
+    while True:
+        # Store the current state in the policy grid
+        x, y = divmod(curr_state, 4)
+        # Determine the best action to take in the current state
+        policy[x][y] = 1
+        # Determine the best action to take in the current state
+        next_action = greedy_policy(curr_state, U)
+        # Get the next possible states that we could transition to
+        next_possible_states = list(get_next_states(curr_state, next_action))
+        # Choose the next state with the highest utility value
+        curr_state = next_possible_states[np.argmax(
+            [U[state] for state in next_possible_states])]
+
+        # If we have reached the goal state, store it in the policy grid and break out of the loop
+        if curr_state == 15:
+            policy[3][3] = 1
+            break
+
+    # Create a heatmap of the policy grid using Seaborn
+    sns.heatmap(policy, annot=True, fmt=".0f", cmap="YlGnBu")
+
+    # Add a title to the plot
+    plt.title("Greedy Policy")
+
+    # Show the plot
+    plt.show()
+
+
 if __name__ == '__main__':
-    # TODO: Your code goes here. You can define additional
-    # functions and variables anywhere in the file as you see fit.
-    pass
+    utilities = value_iteration()
+    display_utilities(utilities)
+    display_greedy_policy(utilities)
